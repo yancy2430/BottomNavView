355dfbe131a60ccdf8eb22313030ff262391587f
yangzhe <yancy@tdeado.com>
2019-08-02 22:03:43 +0800

    升级为Androidx

--- app/src/main/java/com/tdeado/bottomnavview/MainActivity.java
+++ app/src/main/java/com/tdeado/bottomnavview/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tdeado.bottomnavview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.tdeado.bottomnav.BottomItem;
 import com.tdeado.bottomnav.BottomMenuView;
